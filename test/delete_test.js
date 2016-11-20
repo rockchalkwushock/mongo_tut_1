aaa67e1de818c583f51fd5b0022fb9909f8f4562
Cody Brunner <rockchalkwushock@icloud.com>
2016-11-19 23:08:22 -0600

    Added delete test.

---
+++ test/delete_test.js
@@ -0,0 +1,48 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save()
+      .then(() => done());
+  });
+
+  // NOTE: This will use our instance: joe.
+  it('model instance remove', (done) => {
+    joe.remove()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      })
+  });
+
+  // NOTE: These will use User.
+  it('class method remove', (done) => {
+    User.remove({ name: 'Joe' })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      })
+  });
+  it('class method findOneAndRemove', (done) => {
+    User.findOneAndRemove({ name: 'Joe' })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      })
+  });
+  it('class method findByIdAndRemove', (done) => {
+    User.findByIdAndRemove(joe._id)
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      })
+  });
+});

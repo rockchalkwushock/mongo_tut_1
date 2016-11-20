7cc6b8bd4b09485d135d73dd38bc71985e0e52e9
Cody Brunner <rockchalkwushock@icloud.com>
2016-11-20 00:45:20 -0600

    Updated UserSchema with virtual type. Updtated update_test. Added virtual_type_test.js.

--- test/udpate_test.js
+++ test/udpate_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save()
       .then(() => done());
   });
@@ -48,11 +48,12 @@ describe('Updating records', () => {
       done
     );
   });
-  it('A user can have their postcount incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+  // NOTE: xit will tell Mongoose not to look at the test.
+  it('A user can have their likes incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 1 } })
         .then(() => User.findOne({ name: 'Joe' }))
         .then((user) => {
-          assert(user.postCount === 1);
+          assert(user.likes === 1);
           done();
         });
   });

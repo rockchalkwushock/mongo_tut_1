4cd9d091ad02efc89a5bfbac334fc516b77010ed
Cody Brunner <rockchalkwushock@icloud.com>
2016-11-20 02:39:42 -0600

    Updated reading_test with skip(), limit(), & sort().

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-  let joe;
+  let alex, joe, maria, zach;
 
   beforeEach((done) => {
+    alex = new User({ name: 'Alex' });
     joe = new User({ name: 'Joe' });
-    joe.save()
+    maria = new User({ name: 'Maria' });
+    zach = new User({ name: 'Zach' });
+
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
     .then(() => done());
   });
 
@@ -26,5 +30,18 @@ describe('Reading users out of the database', () => {
           assert(user.name === 'Joe');
           done();
         });
+  });
+  it('can skip and limit the result set', (done) => {
+    // -Alex- [Joe Maria] Zach
+    User.find({})
+      .sort({ name: 1 }) // sort in an ascending order A - Z.
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === 'Joe');
+        assert(users[1].name === 'Maria');
+        done();
+      })
   });
 });
